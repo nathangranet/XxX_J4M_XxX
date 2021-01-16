9bb0049aee9a6f96e8bf2c1bc9cca2976eca5261
nathan.granet@epitech.eu <nathan.granet@epitech.eu>
2021-01-16 17:19:59 +0100

    a

--- jeu.gd
+++ jeu.gd
@@ -1,7 +1,23 @@
 extends Node2D
 
+var new = true
+var buy = 0
+var don = 0
+
+func _ready():
+	randomize()
+
 func _process(delta):
+	if $bonome.position.x == 1000 and new == true:
+		new = false
+		var buy = rand_range(1, 200)
+		buy = int(buy * 100) / 100.00
+		$acheter.text = String(buy)
+		don = rand_range(buy, buy + 30)
+		don = int(don * 100) / 100.00
+		$don.text = String(don)
 	if $bonome.position.x == -500:
+		new = true
 		$AnimationPlayer.play("débarque_dans_le_game")
 	if Input.is_action_just_pressed("pay") and $bonome.position.x == 1000:
 		$AnimationPlayer.play("se_casse")
