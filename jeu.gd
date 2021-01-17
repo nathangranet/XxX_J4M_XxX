fcc8ab0611128fa52cab577da9a43c3cc698787d
nathan.granet@epitech.eu <nathan.granet@epitech.eu>
2021-01-17 12:06:13 +0100

    a'

--- jeu.gd
+++ jeu.gd
@@ -3,14 +3,34 @@ extends Node2D
 var new = true
 var buy = 0
 var don = 0
+var rendu = 0
+var score = 0
 
 func _ready():
 	randomize()
 
 func _process(delta):
+	if Input.is_action_just_pressed("1cent"):
+		rendu += 0.01
+	if Input.is_action_just_pressed("5cent"):
+		rendu += 0.05
+	if Input.is_action_just_pressed("10cent"):
+		rendu += 0.10
+	if Input.is_action_just_pressed("50cent"):
+		rendu += 0.50
+	if Input.is_action_just_pressed("1dol"):
+		rendu += 1
+	if Input.is_action_just_pressed("2dol"):
+		rendu += 2
+	if Input.is_action_just_pressed("10dol"):
+		rendu += 10
+	if Input.is_action_just_pressed("50dol"):
+		rendu += 50
+	if Input.is_action_just_pressed("100dol"):
+		rendu += 100
 	if $bonome.position.x == 1000 and new == true:
 		new = false
-		var buy = rand_range(1, 200)
+		buy = rand_range(1, 200)
 		buy = int(buy * 100) / 100.00
 		$acheter.text = String(buy)
 		don = rand_range(buy, buy + 30)
@@ -20,4 +40,13 @@ func _process(delta):
 		new = true
 		$AnimationPlayer.play("débarque_dans_le_game")
 	if Input.is_action_just_pressed("pay") and $bonome.position.x == 1000:
+		if rendu >= (don - buy):
+			score += 10 - (rendu - (don-buy))
+		else:
+			score -= 10
+		print (rendu)
+		print (don - buy)
+		print (buy)
+		rendu = 0
+		$score2.text = String(score)
 		$AnimationPlayer.play("se_casse")
